6ceab8d8c29cedb79954131f85872bbad137825f
roca <RomelCampbell@gmail.com>
2015-12-06 14:51:08 -0500

    Usses jspm bundle with inject

--- solitaire-systemjs-course/app/config.js
+++ solitaire-systemjs-course/app/config.js
@@ -15,6 +15,36 @@ System.config({
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
+  bundles: {
+    "app.bundle.js": [
+      "app.js",
+      "github:angular/bower-angular@1.4.8",
+      "github:fatlinesofcode/ngDraggable@0.1.8",
+      "klondike/klondike.js",
+      "github:angular/bower-angular@1.4.8/angular",
+      "klondike/game.js",
+      "klondike/board.js",
+      "klondike/scoring.js",
+      "github:fatlinesofcode/ngDraggable@0.1.8/ngDraggable",
+      "npm:underscore@1.8.3",
+      "cards/deck.js",
+      "github:angular/bower-angular-route@1.4.8",
+      "klondike/piles/foundationPile.js",
+      "npm:babel-runtime@5.8.34/helpers/class-call-check",
+      "npm:babel-runtime@5.8.34/helpers/create-class",
+      "klondike/piles/tableauPile.js",
+      "klondike/piles/remainderPile.js",
+      "cards/card.js",
+      "klondike/piles/pile.js",
+      "npm:babel-runtime@5.8.34/core-js/object/create",
+      "npm:babel-runtime@5.8.34/core-js/object/define-property",
+      "npm:underscore@1.8.3/underscore",
+      "github:angular/bower-angular-route@1.4.8/angular-route",
+      "npm:core-js@1.2.6/library/fn/object/create",
+      "npm:core-js@1.2.6/library/fn/object/define-property",
+      "npm:core-js@1.2.6/library/modules/$"
+    ]
+  },
 
   map: {
     "angular": "github:angular/bower-angular@1.4.8",
